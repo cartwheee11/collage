29a06e56494499a2306404ecb211997d68430e4f
cartwheee11 <cartwheel@MBP-Cartwheel.Dlink>
2021-09-13 14:59:25 +0300

    adding eslint and prettier

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,19 @@
+const fs = require("fs");
+const collage = require("../src/index");
 
-// import collage from '../examples/src/index.js'
-
-let collage = require('../src/index.js');
-let fs = require('fs');
-
-let buffer = collage({
+const buffer = collage({
 	images: [
-		'./examples/images/1.jpg',
-		'./examples/images/2.jpg',
-		'./examples/images/3.jpg',
-		'./examples/images/4.jpg',
+		"./examples/images/1.jpg",
+		"./examples/images/2.jpg",
+		"./examples/images/3.jpg",
+		"./examples/images/4.jpg",
 	],
 	cols: 3,
 	gap: 10,
-})
+	background: 0xffffffff,
+});
 
 // console.log(buffer)
-buffer.then(buffer => {
-	fs.writeFileSync('./examples/collage.png', buffer);
-})
+buffer.then((currentBuffer) => {
+	fs.writeFileSync("./examples/collage.png", currentBuffer);
+});
--- src/index.js
+++ src/index.js
@@ -1,73 +1,79 @@
-let Jimp = require('jimp');
+const Jimp = require("jimp");
 
 async function collage(tmp) {
-	let defs = {
-		gap: 30,
-		width: 1000,
-		images: [],
-		image: null,
-		cols: 2
-	}
-	let opts = {};
-	Object.assign(opts, defs, tmp);
+  const defs = {
+    gap: 30,
+    width: 1000,
+    images: [],
+    image: null,
+    cols: 2,
+  };
+  const opts = {};
+  Object.assign(opts, defs, tmp);
 
-	if (opts.image) {
-		opts.images.push(image);
-	}
+  if (opts.image) {
+    opts.images.push(opts.image);
+  }
 
-	let rowsNumber = Math.ceil(opts.images.length / opts.cols);
-	let rows = [];
+  const rowsNumber = Math.ceil(opts.images.length / opts.cols);
+  const rows = [];
 
-	//making rows from images array
-	for (let i = 0; i < rowsNumber; i++) {
-		rows[i] = {};
-		rows[i].images = [];
+  //  making rows from images array
+  for (let i = 0; i < rowsNumber; i += 1) {
+    rows[i] = {};
+    rows[i].images = [];
 
-		for (let j = 0; j < opts.cols; j++) {
-			if (opts.images.length)
-				rows[i].images[j] = await Jimp.read(opts.images.shift());
-		}
+    for (let j = 0; j < opts.cols; j += 1) {
+      if (opts.images.length) {
+        rows[i].images[j] = Jimp.read(opts.images.shift());
+      }
+    }
 
-		//computing row hegiht
-		let devisor = 0;
-		rows[i].images.forEach(image => {
-			devisor += (image.bitmap.width) / (image.bitmap.height);
+    rows[i].images = await Promise.all(rows[i].images);
 
-		})
-		// console.log(rows[i].images.length)
-		rows[i].height = (opts.width - opts.gap * rows[i].images.length) / devisor;
-	}
+    //  computing row hegiht
+    let devisor = 0;
+    rows[i].images.forEach((image) => {
+      devisor += image.bitmap.width / image.bitmap.height;
+    });
 
-	//making canvas
-	let heights = rows.map(row => row.height)
-	let canvasHeight = heights.reduce((sum, height) => sum + height);
+    rows[i].height = (opts.width - opts.gap * rows[i].images.length) / devisor;
+  }
 
-	let buffer = new Promise((res, rej) => {
-		new Jimp(opts.width + opts.gap, canvasHeight + opts.gap * rows.length + opts.gap, (err, canvas) => {
+  //  making canvas
+  const heights = rows.map((row) => row.height);
+  const canvasHeight = heights.reduce((sum, height) => sum + height);
 
-			let heightOffset = opts.gap;
+  const buffer = new Promise((res) => {
+    const width = opts.width + opts.gap;
+    const height = canvasHeight + opts.gap * rows.length + opts.gap;
 
-			rows.forEach((row, i) => {
-				let lastRow = i == rows.length - 1;
+    new Jimp(width, height, (err, canvas) => {
+      let heightOffset = opts.gap;
 
-				let widthOffset = opts.gap;
+      rows.forEach((row, i) => {
+        let widthOffset = opts.gap;
 
-				row.images.forEach((image, j) => {
-					image.resize(Jimp.AUTO, row.height)
-					canvas.composite(image, widthOffset + opts.gap * j, heightOffset + opts.gap * i)
-					widthOffset += image.bitmap.width
-				})
+        row.images.forEach((image, j) => {
+          image.resize(Jimp.AUTO, row.height);
+          canvas.composite(
+            image,
+            widthOffset + opts.gap * j,
+            heightOffset + opts.gap * i
+          );
+          widthOffset += image.bitmap.width;
+        });
 
-				heightOffset += row.height;
-			})
-			canvas.getBuffer(Jimp.AUTO, (error, buffer) => {
-				res(buffer);
-			})
+        heightOffset += row.height;
+      });
 
-		})
-	});
+      canvas.getBuffer(Jimp.AUTO, (error, currentBuffer) => {
+        res(currentBuffer);
+      });
+    });
+  });
 
-	return buffer;
+  return buffer;
 }
 
 module.exports = collage;
